4dc86a94d1e63be2037eb23e35f043c2f93b2c1a
fbzr <fabricio.spb@gmail.com>
2020-03-27 08:40:22 -0700

    create login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import Login from "./components/Login";
 import "./styles.scss";
+// components
+import ProtectedRoute from './components/ProtectedRoute';
+import Login from "./components/Login";
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <ProtectedRoute exact path="/bubbles" component={BubblePage} />
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { login } from '../crud/auth';
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const { push } = useHistory();
+  const [user, setUser] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    e.persist();
+    setUser(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value
+    }));
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const res = await login(user);
+      console.log(res);
+      localStorage.setItem('token', res.data.payload);
+      push('/bubbles');
+    } catch(err) {
+      console.log(err);
+    }
+    
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='username'>Username:</label>
+      <input onChange={handleChange} name='username' id='username' type='text' />
+
+      <label htmlFor='password'>Password:</label>
+      <input onChange={handleChange} name='password' id='password' type='password' />
+
+      <button type='submit'>Log in</button>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </form>
   );
 };
 
